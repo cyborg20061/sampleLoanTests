ca007f6e8152d8dfe587fd81cc45e8309ee51df1
meaghanlewis <moneikmarie@gmail.com>
2015-08-26 09:17:49 -0700

    update run selenium script

--- runSelenium.sh
+++ runSelenium.sh
@@ -4,7 +4,7 @@ cd ui_automation_copy
 #download and start sauce connect
 curl -O https://saucelabs.com/downloads/sc-latest-linux.tar.gz
 tar -xzf sc-latest-linux.tar.gz
-cd sc-*-linux && ./bin/sc -u meaghanlewis -k cc32e901-ac33-49e1-8f8f-2aa3ccc8ea4a > /dev/null 2>&1 &
+cd sc-*-linux && ./bin/sc -u $SAUCE_USERNAME -k $SAUCE_ACCESS_KEY > /dev/null 2>&1 &
 
 # Clean and build the project
 mvn clean compile
