99889aaa77aedf65561070eb6597001364427ddb
meaghanlewis <moneikmarie@gmail.com>
2015-08-26 09:28:20 -0700

    update no ssl bump when starting sauce connect

--- runSelenium.sh
+++ runSelenium.sh
@@ -4,7 +4,7 @@ cd ui_automation_copy
 #download and start sauce connect
 curl -O https://saucelabs.com/downloads/sc-latest-linux.tar.gz
 tar -xzf sc-latest-linux.tar.gz
-cd sc-*-linux && ./bin/sc -u $SAUCE_USERNAME -k $SAUCE_ACCESS_KEY > /dev/null 2>&1 &
+cd sc-*-linux && ./bin/sc -u $SAUCE_USERNAME -k $SAUCE_ACCESS_KEY -B *.meetearnest.com > /dev/null 2>&1 &
 
 # Clean and build the project
 mvn clean compile
