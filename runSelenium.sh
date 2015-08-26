06d5439bb9beb8049dab995c860441ac452565c4
meaghanlewis <moneikmarie@gmail.com>
2015-08-25 22:55:14 -0700

    run selenium script

--- runSelenium.sh
+++ runSelenium.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 cd ui_automation_copy
+
+#download and start sauce connect
+curl -O https://saucelabs.com/downloads/sc-latest-linux.tar.gz
+tar -xzf sc-latest-linux.tar.gz
+cd sc-*-linux && ./bin/sc -u meaghanlewis -k cc32e901-ac33-49e1-8f8f-2aa3ccc8ea4a > /dev/null 2>&1 &
+
+# Clean and build the project
+mvn clean compile
 mvn -Dtest=SmokeTestSuite test
